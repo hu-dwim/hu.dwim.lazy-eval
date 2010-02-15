896f6d33b124824952a17452fb8ff44cf23bd900
levente.meszaros <levente.meszaros@gmail.com>
2010-02-15 17:41:07 +0000

    Some more test cases for primes related stuff.

--- test/prime.lisp
+++ test/prime.lisp
@@ -31,5 +31,9 @@
   (sieve (integers-from 2)))
 
 (def test test/primes ()
+  (is (equal (with-lazy-eval (take (integers-from 0) 10))
+             '(0 1 2 3 4 5 6 7 8 9)))
+  (is (equal (with-lazy-eval (take (strike-off 2 (integers-from 0)) 10))
+             '(1 3 5 7 9 11 13 15 17 19)))
   (is (equal (with-lazy-eval (take (primes) 10))
              '(2 3 5 7 11 13 17 19 23 29))))
